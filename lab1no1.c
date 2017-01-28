578f5ba6726849ec1cd9822ecfcb4b7201cf148a
Mr.J <worapol.n@ku.th>
2017-01-28 15:39:29 +0700

    add lab1no1.c

---
+++ lab1no1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void main()
+{
+    int hour, min;
+    printf("Enter hour: ");
+    scanf("%d", hour);
+    printf("Enter minute: ");
+    scanf("%d", min);
+    printf("Time is %d:%d", hour, min);
+}
